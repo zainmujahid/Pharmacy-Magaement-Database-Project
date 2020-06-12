19ffe78aecea280d2f54a5a34988cba432c4ed4f
zainmujahid <zainmujahid01@gmail.com>
2020-06-12 22:24:43 +0500

    created table Customer

--- EE436 Database Creation.sql
+++ EE436 Database Creation.sql
@@ -9,3 +9,15 @@ Annas Bin Malik		(2016-EE-337)
 DROP DATABASE IF EXISTS Pharmacy;
 CREATE DATABASE IF NOT EXISTS Pharmacy;
 USE Pharmacy;
+
+CREATE TABLE `Customer` (
+    SSN INT(10) NOT NULL,
+    `First Name` CHAR(255) NOT NULL,
+    `Last Name` CHAR(255) NOT NULL,
+    Phone INT(10) NOT NULL UNIQUE,
+    Gender CHAR(1) NOT NULL,
+    Address CHAR(255) NOT NULL,
+    `Date of Birth` DATE NOT NULL,
+    `Insurance ID` INT(10) NULL UNIQUE,
+    PRIMARY KEY (SSN)
+)  ENGINE=INNODB;
