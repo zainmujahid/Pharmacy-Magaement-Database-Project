e14355826caad3c695da4f19ffea6d0cfbd256f6
zainmujahid <zainmujahid01@gmail.com>
2020-06-12 22:23:01 +0500

    added info of group members

---
+++ EE436 Database Creation.sql
@@ -0,0 +1,7 @@
+/*
+EE-436 Database Engineering - Pharmacy Management System
+Zain Mujahid 		(2016-EE-304)
+Samiullah Arshad 	(2016-EE-276)
+Fatimah Lyba Khan 	(2016-EE-342)
+Annas Bin Malik		(2016-EE-337)
+*/
