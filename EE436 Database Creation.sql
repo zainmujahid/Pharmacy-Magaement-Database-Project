4e9d39a1a52faee02b50197f42d54eb52e80f786
zainmujahid <zainmujahid01@gmail.com>
2020-06-12 22:23:59 +0500

    created database Pharmacy, and made it default for use

--- EE436 Database Creation.sql
+++ EE436 Database Creation.sql
@@ -5,3 +5,7 @@ Samiullah Arshad 	(2016-EE-276)
 Fatimah Lyba Khan 	(2016-EE-342)
 Annas Bin Malik		(2016-EE-337)
 */
+
+DROP DATABASE IF EXISTS Pharmacy;
+CREATE DATABASE IF NOT EXISTS Pharmacy;
+USE Pharmacy;
